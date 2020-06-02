d3a31622ef5346508d8c05a0dc5856e4ef583b1b
Mahitha B.S.L <30821613+mahithabsl@users.noreply.github.com>
2020-06-02 22:44:10 +0530

    Updated navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,49 @@
 import React from "react";
-import { Link, NavLink, withRouter } from "react-router-dom";
-const Navbar = props => {
-  // console.log(props);
-  // setTimeout(() => {
-  //   props.history.push("/about");
-  // }, 2000);
+import * as ReactBootStrap from "react-bootstrap";
+
+const Navbar = () => {
   return (
-    <nav className="nav-wrapper red darken-3">
-      <div className="container">
-        <a href="/" className="brand-logo left">
-          Demo
-        </a>
-        <ul className="right">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <NavLink to="/contact">Contact</NavLink>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <ReactBootStrap.Navbar
+      className="navbar-styles"
+      collapseOnSelect
+      expand="lg"
+      bg="danger"
+      variant="dark"
+    >
+      <ReactBootStrap.Navbar.Brand href="#home" className="red-heading">
+        Top headlines of the day !
+      </ReactBootStrap.Navbar.Brand>
+
+      <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+        <ReactBootStrap.Nav className="mr-auto">
+          {/* <ReactBootStrap.Nav.Link href="#features">Features</ReactBootStrap.Nav.Link> */}
+          {/* <ReactBootStrap.Nav.Link href="#pricing">Pricing</ReactBootStrap.Nav.Link> */}
+        </ReactBootStrap.Nav>
+
+        <ReactBootStrap.Nav className="link-styles">
+          <ReactBootStrap.Nav.Link href="/" className="red-text">
+            <b>General</b>
+          </ReactBootStrap.Nav.Link>
+          <ReactBootStrap.Nav.Link href="/health" className="red-text">
+            <b> Health</b>
+          </ReactBootStrap.Nav.Link>
+          <ReactBootStrap.Nav.Link href="/science" className="red-text">
+            <b> Science</b>
+          </ReactBootStrap.Nav.Link>
+          <ReactBootStrap.Nav.Link href="/sports" className="red-text">
+            <b> Sports</b>
+          </ReactBootStrap.Nav.Link>
+          <ReactBootStrap.Nav.Link href="/entertain" className="red-text">
+            <b> Entertainment</b>
+          </ReactBootStrap.Nav.Link>
+          <ReactBootStrap.Nav.Link href="/tech" className="red-text">
+            <b> Technology</b>
+          </ReactBootStrap.Nav.Link>
+        </ReactBootStrap.Nav>
+      </ReactBootStrap.Navbar.Collapse>
+    </ReactBootStrap.Navbar>
   );
 };
 
-export default withRouter(Navbar);
+export default Navbar;
